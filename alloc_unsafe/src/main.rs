386b7f05108afe1bf834962ee7747867bead8235
Trevor Carlson <trevorac99@gmail.com>
2021-01-15 22:47:44 -0600

    refactor code in alloc_unsafe

--- alloc_unsafe/src/main.rs
+++ alloc_unsafe/src/main.rs
@@ -1,4 +1,5 @@
 use std::alloc::{alloc, dealloc, Layout};
+use std::mem::{align_of, size_of};
 
 struct Point {
     x: f64,
@@ -33,39 +34,54 @@ unsafe fn example_alloc_single_point() {
     dealloc(heap_point as *mut u8, Layout::new::<Point>());
 }
 
-unsafe fn example_alloc_multi_points() {
+/// Custom alloc function that makes the current allocation API
+/// a bit nicer.
+unsafe fn my_alloc<T>(num: usize) -> *mut T {
+    let layout =
+        Layout::from_size_align(size_of::<T>() * num, align_of::<T>()).expect("Alignment error");
+    alloc(layout) as *mut T
+}
+
+/// Custom dealloc function that makes the current allocation API
+/// a bit nicer.
+unsafe fn my_dealloc<T>(ptr: *mut T, num: usize) {
+    let layout =
+        Layout::from_size_align(size_of::<T>() * num, align_of::<T>()).expect("Alignment error");
+    dealloc(ptr as *mut u8, layout)
+}
+
+fn example_alloc_multi_points() {
     // Create an array of items allocated on the stack.
     let stack_points = [Point::origin(), Point::new(3.0, 4.0), Point::new(6.0, 8.0)];
 
     let number_of_elements = stack_points.len();
 
-    let layout = Layout::from_size_align_unchecked(
-        std::mem::size_of::<Point>() * number_of_elements,
-        std::mem::align_of::<Point>(),
-    );
+    // We can limit unsafe code to a specific block and only include
+    // the code that depends on unsafe operations.
+    unsafe {
+        let heap_points_raw: *mut Point = my_alloc(number_of_elements);
 
-    let heap_points_raw: *mut Point = alloc(layout) as *mut Point;
+        let heap_points_slice = std::slice::from_raw_parts_mut(heap_points_raw, number_of_elements);
 
-    let heap_points_slice = std::slice::from_raw_parts_mut(heap_points_raw, number_of_elements);
+        for (index, point) in heap_points_slice.iter_mut().enumerate() {
+            let index_plus_1 = (index + 1) as f64;
+            // Here `*point` is dereferencing a normal Rust mutable reference, not a raw pointer so this line is actually safe code
+            *point = Point::new(3.0 * index_plus_1, 4.0 * index_plus_1);
+        }
 
-    for (index, point) in heap_points_slice.iter_mut().enumerate() {
-        let index_plus_1 = (index + 1) as f64;
-        // Here `*point` is dereferencing a normal Rust mutable reference, not a raw pointer so this line is actually safe code
-        *point = Point::new(3.0 * index_plus_1, 4.0 * index_plus_1);
+        for index in 0..number_of_elements {
+            let distance = stack_points[index].distance(&heap_points_slice[index]);
+
+            println!(
+                "Distance betweens points at index {} is {}",
+                index, distance
+            );
+        }
+
+        drop(heap_points_slice);
+
+        my_dealloc(heap_points_raw, number_of_elements);
     }
-
-    for index in 0..number_of_elements {
-        let distance = stack_points[index].distance(&heap_points_slice[index]);
-
-        println!(
-            "Distance betweens points at index {} is {}",
-            index, distance
-        );
-    }
-
-    drop(heap_points_slice);
-
-    dealloc(heap_points_raw as *mut u8, layout);
 }
 
 fn main() {
